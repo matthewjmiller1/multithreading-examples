3ff8240c0f923fa816bf498c191b75091b75a491
Matt Miller <matt@matthewjmiller.net>
2017-01-23 22:44:53 -0500

    Add TODO comments to c_pthreads

--- c_pthreads/c_pthreads.c
+++ c_pthreads/c_pthreads.c
@@ -1,6 +1,14 @@
+#include <pthread.h>
+#include <stdio.h>
+
+/*
+ * TODO:
+ * - Producer thread increments widgets by N, waits for all to be handled.
+ * - M consumer threads consume X widgets as long as some are available.
+ */
 
 int
-main(void)
+main(int argc, char **argv)
 {
     return 0;
 }
