5ef136000a431888bc8de8f5841658868c1410a0
Matt Miller <matt@matthewjmiller.net>
2017-04-28 22:01:55 -0400

    Change threading logic

--- c_pthreads/counting/counting.c
+++ c_pthreads/counting/counting.c
@@ -6,43 +6,47 @@
 static pthread_mutex_t mtx;
 static pthread_cond_t cv;
 
+static uint8_t printer_tid = 0;
 static uint8_t cur_count = 0;
+static bool cur_count_handled = false;
 static const uint8_t TOTAL_COUNT = 50;
-static uint8_t thread_ready_count = 0;
+
+static const bool DO_DEBUG = false;
 
 static void *
 counting_fn(void *ctx)
 {
     uint8_t id = (uint8_t) ctx;
 
-    printf("%s: starting thread %u\n", __func__, id);
+    if (DO_DEBUG) {
+        printf("%s: starting thread %u\n", __func__, id);
+    }
 
     pthread_mutex_lock(&mtx);
-    ++thread_ready_count;
-    pthread_cond_broadcast(&cv);
-    pthread_mutex_unlock(&mtx);
-
-    pthread_mutex_lock(&mtx);
-    while (cur_count <= TOTAL_COUNT) {
-        if ((1 == id) && (0 != (cur_count % 2))) {
+    while (true) {
+        if (id == printer_tid) {
             printf("%s: (thread %u) cur_count=%u\n", __func__, id, cur_count);
-        } else if ((2 == id) && (0 == (cur_count % 2))) {
-            printf("%s: (thread %u) cur_count=%u\n", __func__, id, cur_count);
+            cur_count_handled = true;
+            pthread_cond_signal(&cv);
+        }
+        if (cur_count >= TOTAL_COUNT) {
+            break;
         }
         pthread_cond_wait(&cv, &mtx);
     }
     pthread_mutex_unlock(&mtx);
 
-    printf("%s: finishing thread %u\n", __func__, id);
+    if (DO_DEBUG) {
+        printf("%s: finishing thread %u\n", __func__, id);
+    }
     pthread_exit(NULL);
 }
 
 int
 main(int argc, char **argv)
 {
-    static const uint8_t THREAD_COUNT = 2;
+    static const uint8_t THREAD_COUNT = 5;
     pthread_t threads[THREAD_COUNT];
-    bool do_count_loop = true;
     uint8_t i;
 
     pthread_mutex_init(&mtx, NULL);
@@ -54,19 +58,17 @@ main(int argc, char **argv)
     }
 
     pthread_mutex_lock(&mtx);
-    while (thread_ready_count < THREAD_COUNT) {
-        pthread_cond_wait(&cv, &mtx);
+    printer_tid = 0;
+    for (cur_count = 1; cur_count <= TOTAL_COUNT; ++cur_count) {
+        printer_tid = (printer_tid >= THREAD_COUNT) ? 1 : (printer_tid + 1);
+        cur_count_handled = false;
+        printf("%s: cur_count=%u\n", __func__, cur_count);
+        pthread_cond_broadcast(&cv);
+        while (!cur_count_handled) {
+            pthread_cond_wait(&cv, &mtx);
+        }
     }
     pthread_mutex_unlock(&mtx);
-
-    while (do_count_loop) {
-        pthread_mutex_lock(&mtx);
-        ++cur_count;
-        printf("%s: cur_count=%u\n", __func__, cur_count);
-        do_count_loop = (cur_count <= TOTAL_COUNT);
-        pthread_cond_broadcast(&cv);
-        pthread_mutex_unlock(&mtx);
-    }
 
     for (i = 0; i < THREAD_COUNT; ++i) {
         pthread_join(threads[i], NULL);
