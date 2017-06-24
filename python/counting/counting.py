61dc97797a4358d7f7567a780a4a2d37b83140b7
Matt Miller <matt@matthewjmiller.net>
2017-06-23 22:50:34 -0400

    Add more logic for Python counting

--- python/counting/counting.py
+++ python/counting/counting.py
@@ -4,22 +4,84 @@
 """Example of multi-threading in Python for handling counting."""
 
 import threading
+import sys
 
-class ThreadCtx:
-    """Context to be used in threads"""
+class CommonCtx:
+    """Common context to be used in threads"""
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, total_count, do_debug = False):
+        self.total_count = total_count
+        self.do_debug = do_debug
+        self.printer_tid = 0
+        self.cur_count = 0
+        self.cur_count_handled = False
+        self.lock = threading.Lock()
+        self.cv = threading.Condition(self.lock)
 
     def __repr__(self):
-        print "ThreadCtx {}".format(self.id)
+        return "(total_count={}, ".format(self.total_count) + \
+            "do_debug={}, printer_tid={} ".format(self.do_debug,
+                                                  self.printer_tid) + \
+            "cur_count={}, cur_count_handled={})".format(self.cur_count,
+                                                         self.cur_count_handled)
 
-def counting_fn(ctx):
-    print "ID: {}".format(ctx.id)
+class ThreadCtx:
+    """Thread-specific context to be used in threads"""
 
-lock = threading.Lock()
-cv = threading.Condition(lock)
+    def __init__(self, id, common_ctx):
+        self.id = id
+        self.common_ctx = common_ctx
 
-ctx = ThreadCtx(1)
-t = threading.Thread(target=counting_fn, args=(ctx,))
-t.start()
+    def __repr__(self):
+        return "(id={}, common_ctx={})".format(self.id, self.common_ctx)
+
+def tprint(str):
+    """Utility for thread-safe printing"""
+
+    sys.stdout.write(str + "\n")
+
+def counting_fn(thread_ctx):
+    """Thread counting function"""
+
+    ctx = thread_ctx.common_ctx
+    id = thread_ctx.id
+
+    if (ctx.do_debug):
+        tprint("starting thread {}".format(id))
+
+    with ctx.cv:
+        if (ctx.do_debug):
+            tprint("(thread {}) got lock".format(id))
+        while (True):
+            if (id is ctx.printer_tid):
+                tprint("(thread {}) cur_count={}".format(id, ctx.cur_count))
+                ctx.cur_count_handled = True
+                ctx.cv.notify()
+            if (ctx.cur_count >= ctx.total_count):
+                break
+            ctx.cv.wait()
+
+    if (ctx.do_debug):
+        tprint("finishing thread {}".format(id))
+
+THREAD_COUNT = 5
+#ctx = CommonCtx(50)
+ctx = CommonCtx(50, True)
+
+for i in range(THREAD_COUNT):
+    thread_ctx = ThreadCtx((i + 1), ctx)
+    t = threading.Thread(target=counting_fn, args=(thread_ctx,))
+    t.start()
+
+with ctx.cv:
+    ctx.printer_tid = 0
+    for ctx.cur_count in range(1, ctx.total_count):
+        ctx.printer_tid = \
+            1 if (ctx.printer_tid >= THREAD_COUNT) else (ctx.printer_tid + 1)
+        ctx.cur_count_handled = False
+        tprint("cur_count={}".format(ctx.cur_count))
+        ctx.cv.notify_all()
+        while (not ctx.cur_count_handled):
+            ctx.cv.wait()
+
+tprint("{} threads finished".format(THREAD_COUNT))
